52ba17452b7d9879ff790b7d90ea76bd429ff059
tarun230300 <saitarun.puvvula@ro-one.in>
2023-11-17 15:47:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React,{useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(0);
+  const inputRef = useRef(null); //created for auto focusing 
+  useEffect (() => { inputRef.current.focus();}, []); // for auto focusing 
+
+  const handler = e => {
+    setInput(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="text" value={input} name="input" onChange={handler} ref={inputRef}></input><br></br>
+    
+    <h4>Result: {result}</h4>
+    <button onClick={() => setInput(input+'+')}><b>+</b></button>
+    <button onClick={() => setInput(input+'-')}><b>-</b></button>
+    <button onClick={() => setInput(input+'*')}><b>x</b></button>
+    <button onClick={() => setInput(input+'/')}><b>/</b></button>
+    <button onClick={() => setResult(eval(input))}><b>=</b></button>
+    <button onClick={() => setInput('')}><b>AC</b></button><br />
+    
+    <button onClick={() => setInput(input+'1')}><b>1</b></button>
+    <button onClick={() => setInput(input+'2')}><b>2</b></button>
+    <button onClick={() => setInput(input+'3')}><b>3</b></button>
+    <button onClick={() => setInput(input+'4')}><b>4</b></button>
+    <button onClick={() => setInput(input+'5')}><b>5</b></button>
+    <button onClick={() => setResult('')}><b>C</b></button><br />
+
+    <button onClick={() => setInput(input+'6')}><b>6</b></button>
+    <button onClick={() => setInput(input+'7')}><b>7</b></button>
+    <button onClick={() => setInput(input+'8')}><b>8</b></button>
+    <button onClick={() => setInput(input+'9')}><b>9</b></button>
+    <button onClick={() => setInput(input+'0')}><b>0</b></button>
+    <button onClick={() => setInput(input+'.')}><b>.</b></button><br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
     </div>
   );
 }
